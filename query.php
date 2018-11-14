8ae005617c557eb1646a840167a0ef35ab5ab831
Arie Hoekstra <hoekstraarie@gmail.com>
2018-11-14 10:25:00 +0100

    Dit moest

--- query.php
+++ query.php
@@ -1,15 +1,15 @@
+
 <?php
-
-
-include 'database_connectie.php';
+include 'Database_Connectie.php';
 try {
     $db = db_connect();
     $stmt = $db->query
     ('');
-
     while ($row = $stmt->fetch()) {
         echo $row['StockItemName'] . '<br>';
     }
 } catch (PDOException $a) {
     echo $a->getMessage();
+}
+
 }
