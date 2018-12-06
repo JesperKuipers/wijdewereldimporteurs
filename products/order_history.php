014572d35773b2a52594f5914af11851ea694e38
Johan <j.v.t.hoog@live.nl>
2018-12-06 17:43:04 +0100

    changes

--- products/order_history.php
+++ products/order_history.php
@@ -19,9 +19,9 @@
 
     <?php
     if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
-        $pdo = db_connect();
-        $customerid = $_SESSION['customerid'];
-        $stmt = $pdo->prepare("SELECT oc.orderId, SUM(oc.Quantity*si.RecommendedRetailPrice) total, oc.receivedate, si.stockitemname, ru.customerid
+    $pdo = db_connect();
+    $customerid = $_SESSION['customerid'];
+    $stmt = $pdo->prepare("SELECT oc.orderId, SUM(so.quantity*si.RecommendedRetailPrice) total, oc.receivedate, si.stockitemname, ru.customerid
 FROM orderbycustomers oc 
 JOIN registered_users ru 
 ON oc.customerid = ru.customerid 
@@ -30,52 +30,63 @@ ON oc.orderId = so.orderId
 JOIN stockitems si 
 ON so.StockItemID = si.StockItemID
 WHERE oc.customerid = $customerid
-GROUP BY oc.orderId
+GROUP BY si.stockitemname
 ORDER BY oc.receivedate");
-        $stmt->execute();
-        $customerinfo = $stmt->fetchAll();
-        foreach ($customerinfo as $info) {
+    $stmt->execute();
+    $customerinfo = $stmt->fetchAll();
+    $arrayIds = array();
+    var_dump(array($customerinfo));
+    foreach ($customerinfo
 
-           // $rowarray = $statement->fetchall();
-            //print "<tr>\n";
-            //foreach ($rowarray as $row) {
-               // foreach ($row as $col) {
-                 //   print "\t<td>$col</td>\n";
-             //   }
-                //print "</tr>\n";
-           // }
+             as $info) {
 
-            ?>
-            <div class="container-accountinfo left content">
-            <b>Order</b><br>
-            <?php echo $info['orderId']; ?><br>
 
-            <b>Total</b><br>
-            <?php echo $info['total']; ?><br>
+    // $rowarray = $statement->fetchall();
+    //print "<tr>\n";
+    //foreach ($rowarray as $row) {
+    // foreach ($row as $col) {
+    //   print "\t<td>$col</td>\n";
+    //   }
+    //print "</tr>\n";
+    // }
 
-            <b>Order Date</b><br>
-            <?php echo $info['receivedate']; ?><br>
+    ?>
+    <div class="container-accountinfo left content">
 
-            <b>Address</b><br>
-            <?php echo $info['address']; ?><br>
+<?php if (!in_array($info['orderId'], $arrayIds)){ ?>
 
-            <b>Products</b><br>
-            <?php echo $info['Stockitemname']; ?><br>
-        <?php } ?>
+        <b>Order</b><br>
+        <?php echo $info['orderId']; ?><br>
+
+        <b>Total</b><br>
+        <?php echo $info['total']; ?><br>
+
+        <b>Order Date</b><br>
+        <?php echo $info['receivedate']; ?><br>
+
+        <b>Products</b><br>
+        <?php echo $info['stockitemname']; ?>
+
+        <?php } else {
+            echo $info['stockitemname'];
+        }
+        array_push($arrayIds, $info['orderId']);
+
+
+        } ?>
         <form action="../account/account.php">
             <button type="submit" class="changeaccountbtn s12 btn btn-small waves-effect">Back to account
             </button>
         </form>
-        </div>
-    <?php } else {
-        print("<h3 align='center'>You need to be logged in to see this page.</h3>");
-    }
-    ?>
+        <?php } else {
+            print("<h3 align='center'>You need to be logged in to see this page.</h3>");
+        }
+        ?>
 
-</div>
+    </div>
 
 
-<?php footer() ?>
+    <?php footer() ?>
 
 </body>
 </html>
